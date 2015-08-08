4adbfbc38668184de06733dba74ded22edcb7829
Julius Flohr <julius.flohr@gmail.com>
2015-08-08 14:06:40 +0200

    refactoring, preparation for introduction of vertex buffer objects

--- RetinaOverflow/src/Program.cs
+++ RetinaOverflow/src/Program.cs
@@ -16,16 +16,14 @@ namespace RetinaOverflow
         public static void Main(string[] args)
         {
             GlobalManager.instance.logging.info("Retina Renderer starting");
-            ModelLoader loader = new ModelLoader();
-            var box = loader.loadModel("meshes/sponza.obj");
+            var theWorld = new World();
+            theWorld.addModel("meshes/sponza.obj");
             var camera = new Camera();
+            World.activeCam = camera;
+
             double lastFrameTime = 0;
             double timeSinceLastUpdate = 0;
-            GlobalManager.instance.logging.info(String.Format("View: {0}, {1}, {2}", camera.getViewDirection()[0], camera.getViewDirection()[1], camera.getViewDirection()[2]));
-            GlobalManager.instance.logging.info(String.Format("Up: {0}, {1}, {2}", camera.getUpVector()[0], camera.getUpVector()[1], camera.getUpVector()[2]));
-            camera.move(new Vector3(100, 25, 0));
-           // camera.lookAt(new Vector3(0, 0, 0));
-            //camera.move(new Vector3(-))
+
             MouseState current;
             MouseState previous = Mouse.GetState();
 
@@ -113,23 +111,16 @@ namespace RetinaOverflow
                     // render graphics
                     GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
                     GL.MatrixMode(MatrixMode.Modelview);
-                    var viewMatrix = camera.getViewMatrix();
+                    var viewMatrix = World.activeCam.getViewMatrix();
                     GL.LoadMatrix(ref viewMatrix);
 
                     //GL.Ortho(-10.0, 10.0, -10.0, 10.0, 00.0, 4.0);
                     GL.Color3(1.0f, 0, 0);
 
-                    box.draw();
-					
+                    theWorld.draw();
 
-                    /* GL.Color3(Color.MidnightBlue);
-                        GL.Vertex2(-1.0f, 1.0f);
-                        GL.Color3(Color.SpringGreen);
-                        GL.Vertex2(0.0f, -1.0f);
-                        GL.Color3(Color.Ivory);
-                        GL.Vertex2(1.0f, 1.0f); */
-                    GL.End();
                     GL.PopMatrix();
+                    GL.End();
                     game.SwapBuffers();
                 };
 
--- RetinaOverflow/src/World.cs
+++ RetinaOverflow/src/World.cs
@@ -1,11 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RetinaOverflow
 {
     public class World
     {
+        private List<Model> scene; // TODO: Convert to octree or similar fast data Structure
+        private ModelLoader loader;
+
+
+        public static Camera activeCam;
+
         public World()
         {
+            scene = new List<Model>();
+            loader = new ModelLoader();            
+        }
+
+        public void initializeWorld()
+        {
+
+        }
+
+        public void addModel(ref Model model)
+        {
+            scene.Add(model);
+        }
+
+        public void addModel(String modelPath)
+        {
+            scene.Add(loader.loadModel(modelPath));  
+        }
+
+        public void draw()
+        {
+            foreach (var model in scene)
+            {
+                model.draw();
+            }
         }
     }
 }
